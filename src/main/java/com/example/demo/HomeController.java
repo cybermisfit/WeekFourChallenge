73299db37b211be6aa4fa49f211cf255e77f5d07
Danita <roboticmisfit@gmail.com>
2018-08-03 15:58:44 -0400

    first commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    @RequestMapping("/")
+    public String initialForm(Model model){
+        model.addAttribute("transactions", transactionRepository.findAll());
+        return "transactionhistory";
+    }
+    @GetMapping("/add")
+    public String transactionForm(Model model){
+        model.addAttribute("transaction", new Transaction());
+        return "form";
+    }
+    @PostMapping("/process")
+    public String processForm(@Valid Transaction transaction, BindingResult result){
+        if (result.hasErrors()){
+            return "form";
+        }
+        transactionRepository.save(transaction);
+        return "redirect:/";
+    }
+    @RequestMapping("detail/{id}")
+    public String showTransaction(@PathVariable("id") long id, Model model){
+        model.addAttribute("transaction", transactionRepository.findById(id).get());
+        return "show";
+    }
+//    @RequestMapping("/update/{id}")
+//    public String updateTransaction(@PathVariable("id") long id, Model model){
+//        model.addAttribute("transaction", transactionRepository.findById(id).get());
+//        return "form";
+//    }
+
+}
---
+++ src/main/java/com/example/demo/Transaction.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private int acctnum;
+    private String action;
+    private double amount = 0.00;
+    private String reason;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAcctnum() {
+        return acctnum;
+    }
+
+    public void setAcctnum(int acctnum) {
+        this.acctnum = acctnum;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
